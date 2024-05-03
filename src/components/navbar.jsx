0d7cf61e6a56048efd1626d6f4a8997b807a09db
unknown <lisdahwati0@gmail.com>
2024-05-03 21:04:49 +0700

    initest

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
       <div className="flex-none">
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <div className="indicator">
+            <div className="indicator bgdark"> 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
