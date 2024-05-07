314cf8eafa70b23ad5c1c41da5a62ca206dbff24
lisdahwati0@gmail.com <lisdahwati0@gmail.com>
2024-05-07 11:52:03 +0700

    jrhiuei

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
-                fill="none
+                fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
